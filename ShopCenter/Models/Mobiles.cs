d711673f43d988c824b11b947cc8c237fc0a4144
Sourav Mondal <souraviit776.bd@gmail.com>
2019-04-01 23:49:43 +0600

    Add files via upload

---
+++ ShopCenter/Models/Mobiles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
+
+namespace ShopCenter.Models
+{
+    public  class Mobiles
+    {
+        public int Slno { get; set; }
+        public string mobileName { get; set; }
+        public Double Price { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ ShopCenter/Models/ProductDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopCenter.Models
+{
+    public class ProductDetails
+    {
+        public int ? Slno { get; set; }
+        public string MobileName { get; set; }
+        public double Price { get; set; }
+        public string Url { get; set; }
+        public string Features { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string SimType { get; set; }
+    }
+}
